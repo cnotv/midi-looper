f18f803b71753473ec906ed0cd17971a1cafca71
Giuseppe Leo <giuseppe.leo@retresco.de>
2021-07-02 23:17:57 +0200

    Keyboard: Fix issue with stuck keys and touch devices

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -6,6 +6,8 @@ interface KeyboardProps {
 }
 
 export const Keyboard = ({ play, current }: KeyboardProps) => {
+  const isTouch = window.navigator.maxTouchPoints > 0;
+
   return (
     <ul className="keyboard" id="keyboard">
       {new Array(107).fill('').map((note, i) =>
@@ -13,8 +15,9 @@ export const Keyboard = ({ play, current }: KeyboardProps) => {
           className={`keyboard__note ${(current[i] > 0) ? ' keyboard__note--active' : ''}`}
           id={`key${i}`}
           key={i}
-          onMouseDown={() => play(`${i}`, 50)}
-          onMouseUp={() => play(`${i}`, 0)}
+          onMouseDown={() => !isTouch ? play(`${i}`, 50) : undefined}
+          onMouseUp={() => !isTouch ? play(`${i}`, 0) : undefined}
+          onMouseLeave={() => !isTouch ? play(`${i}`, 0) : undefined}
           onTouchStart={() => play(`${i}`, 50)}
           onTouchEnd={() => play(`${i}`, 0)}
         >{i}</li>
