6f6d73866f8991b94984f52a4877cb8dd617b08e
Giuseppe Leo <giuseppe.leo@retresco.de>
2021-07-14 17:56:06 +0200

    Disable recording on loop all

--- src/App.tsx
+++ src/App.tsx
@@ -172,7 +172,8 @@ function App() {
     setTracks([
       ...tracks.map(track => ({
         ...track,
-        isLoop: true
+        isLoop: true,
+        isRecording: false
       }))
     ])
   }
